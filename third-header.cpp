dab9e3519cf2a97b0c426b2c1b5a3eda1d608019
minthanhtike <112789258+MinThanHtike2001@users.noreply.github.com>
2022-11-13 00:04:05 +0630

    Add files via upload

---
+++ third-header.cpp
@@ -0,0 +1,16 @@
+#include"second-header.cpp"
+#include"first-header.h"
+#include"iostream"
+#include"stdlib.h"
+
+using namespace N;
+
+int main(){
+    mybank _obj;
+    int ex=_obj.mainpage();
+    if(ex = 0){
+        exit(1);
+    }
+
+
+return 0;}
